cd26cd180c86c90d45fd57279d719e95b5eac37b
BonagucciAI202 <78320452+imgod123456@users.noreply.github.com>
2021-06-06 15:29:21 -0700

    Initialize dev/kawa/605-Search-Experice-Implementation-Add-SearchBox-Control

---
+++ src/cascadia/TerminalControl/SearchBoxControl.cpp
@@ -0,0 +1,90 @@
+﻿//
+// SearchBoxControl.cpp
+// Implementation of the SearchBoxControl class
+//
+
+#include "pch.h"
+#include "SearchBoxControl.h"
+#include "SearchBoxControl.g.cpp"
+
+using namespace winrt;
+using namespace winrt::Windows::UI::Xaml;
+
+namespace winrt::Microsoft::Terminal::TerminalControl::implementation
+{
+    SearchBoxControl::SearchBoxControl() :
+        _goForward(false),
+        _isCaseSensitive(false)
+    {
+        InitializeComponent();
+
+        _goForwardButton = this->FindName(L"SetGoForwardButton").try_as<Controls::Button>();
+        _goBackwardButton = this->FindName(L"SetGoBackwardButton").try_as<Controls::Button>();
+    }
+
+    bool SearchBoxControl::GetGoForward()
+    {
+        return _goForward;
+    }
+
+    bool SearchBoxControl::GetIsCaseSensitive()
+    {
+        return _isCaseSensitive;
+    }
+
+    void SearchBoxControl::AutoSuggestBox_QuerySubmitted(winrt::Windows::Foundation::IInspectable const& /*sender*/, winrt::Windows::UI::Xaml::Controls::AutoSuggestBoxQuerySubmittedEventArgs const& e)
+    {
+        _searchEventHandler(*this, e.QueryText());
+    }
+
+    void SearchBoxControl::_GoBackwardClick(winrt::Windows::Foundation::IInspectable const& /*sender*/, RoutedEventArgs const& /*e*/)
+    {
+        _goForward = false;
+
+        // If button is clicked, we show the blue border around the clicked button, and eliminate
+        // the border on the other direction control button
+        Thickness thickness = ThicknessHelper::FromUniformLength(1);
+        _goBackwardButton.BorderThickness(thickness);
+
+        thickness = ThicknessHelper::FromUniformLength(0);
+        _goForwardButton.BorderThickness(thickness);
+    }
+
+    void SearchBoxControl::_GoForwardClick(winrt::Windows::Foundation::IInspectable const& /*sender*/, RoutedEventArgs const& /*e*/)
+    {
+        _goForward = true;
+
+        // If button is clicked, we show the blue border around the clicked button, and eliminate
+        // the border on the other direction control button
+        Thickness thickness = ThicknessHelper::FromUniformLength(1);
+        _goForwardButton.BorderThickness(thickness);
+
+        thickness = ThicknessHelper::FromUniformLength(0);
+        _goBackwardButton.BorderThickness(thickness);
+    }
+
+    void SearchBoxControl::_MovePositionClick(winrt::Windows::Foundation::IInspectable const& /*sender*/, RoutedEventArgs const& e)
+    {
+        _MovePositionClickedHandler(*this, e);
+    }
+
+    void SearchBoxControl::_CaseSensitivityChecked(winrt::Windows::Foundation::IInspectable const& /*sender*/, RoutedEventArgs const& /*e*/)
+    {
+        _isCaseSensitive = true;
+    }
+
+    void SearchBoxControl::_CaseSensitivityUnChecked(winrt::Windows::Foundation::IInspectable const& /*sender*/, RoutedEventArgs const& /*e*/)
+    {
+        _isCaseSensitive = false;
+    }
+
+    void SearchBoxControl::_CloseClick(winrt::Windows::Foundation::IInspectable const& /*sender*/, RoutedEventArgs const& e)
+    {
+        _CloseButtonClickedHanlder(*this, e);
+    }
+
+    // Events proxies
+    DEFINE_EVENT_WITH_TYPED_EVENT_HANDLER(SearchBoxControl, Search, _searchEventHandler, TerminalControl::SearchBoxControl, winrt::hstring);
+    DEFINE_EVENT_WITH_TYPED_EVENT_HANDLER(SearchBoxControl, CloseButtonClicked, _CloseButtonClickedHanlder, TerminalControl::SearchBoxControl, Windows::UI::Xaml::RoutedEventArgs);
+    DEFINE_EVENT_WITH_TYPED_EVENT_HANDLER(SearchBoxControl, MovePositionClicked, _MovePositionClickedHandler, TerminalControl::SearchBoxControl, Windows::UI::Xaml::RoutedEventArgs);
+}
---
+++ src/cascadia/TerminalControl/SearchBoxControl.h
@@ -0,0 +1,49 @@
+﻿//
+// SearchBoxControl.h
+// Declaration of the SearchBoxControl class
+//
+
+#pragma once
+#include "winrt/Windows.UI.Xaml.h"
+#include "winrt/Windows.UI.Xaml.Controls.h"
+#include "../../cascadia/inc/cppwinrt_utils.h"
+
+#include "SearchBoxControl.g.h"
+#include "../../cascadia/inc/cppwinrt_utils.h"
+
+namespace winrt::Microsoft::Terminal::TerminalControl::implementation
+{
+    struct SearchBoxControl : SearchBoxControlT<SearchBoxControl>
+    {
+        SearchBoxControl();
+
+        void AutoSuggestBox_QuerySubmitted(winrt::Windows::Foundation::IInspectable const& /*sender*/, winrt::Windows::UI::Xaml::Controls::AutoSuggestBoxQuerySubmittedEventArgs const& e);
+
+        bool GetGoForward();
+        bool GetIsCaseSensitive();
+
+        bool _goForward; // The the direction of the search, controlled by the buttons with arrows
+        bool _isCaseSensitive; // If the search should be case sensitive, controlled by the checkbox
+
+        winrt::Windows::UI::Xaml::Controls::Button _goForwardButton;
+        winrt::Windows::UI::Xaml::Controls::Button _goBackwardButton;
+
+        void _GoBackwardClick(winrt::Windows::Foundation::IInspectable const& /*sender*/, winrt::Windows::UI::Xaml::RoutedEventArgs const& /*e*/);
+        void _GoForwardClick(winrt::Windows::Foundation::IInspectable const& /*sender*/, winrt::Windows::UI::Xaml::RoutedEventArgs const& /*e*/);
+        void _MovePositionClick(winrt::Windows::Foundation::IInspectable const& /*sender*/, winrt::Windows::UI::Xaml::RoutedEventArgs const& e);
+        void _CloseClick(winrt::Windows::Foundation::IInspectable const& /*sender*/, winrt::Windows::UI::Xaml::RoutedEventArgs const& e);
+        void _CaseSensitivityChecked(winrt::Windows::Foundation::IInspectable const& /*sender*/, winrt::Windows::UI::Xaml::RoutedEventArgs const& /*e*/);
+        void _CaseSensitivityUnChecked(winrt::Windows::Foundation::IInspectable const& /*sender*/, winrt::Windows::UI::Xaml::RoutedEventArgs const& /*e*/);
+
+        DECLARE_EVENT_WITH_TYPED_EVENT_HANDLER(Search, _searchEventHandler, TerminalControl::SearchBoxControl, winrt::hstring);
+        DECLARE_EVENT_WITH_TYPED_EVENT_HANDLER(CloseButtonClicked, _CloseButtonClickedHanlder, TerminalControl::SearchBoxControl, Windows::UI::Xaml::RoutedEventArgs);
+        DECLARE_EVENT_WITH_TYPED_EVENT_HANDLER(MovePositionClicked, _MovePositionClickedHandler, TerminalControl::SearchBoxControl, Windows::UI::Xaml::RoutedEventArgs);
+    };
+}
+
+namespace winrt::Microsoft::Terminal::TerminalControl::factory_implementation
+{
+    struct SearchBoxControl : SearchBoxControlT<SearchBoxControl, implementation::SearchBoxControl>
+    {
+    };
+}
