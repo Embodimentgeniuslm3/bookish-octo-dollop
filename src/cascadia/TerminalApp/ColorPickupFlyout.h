95bfe2ff5d5c31778a87f82af13af3f2b7dabe01
BonagucciAI202 <78320452+imgod123456@users.noreply.github.com>
2021-06-06 15:29:57 -0700

    Initialize dev/miniksa/tabs-color-fix

---
+++ src/cascadia/TerminalApp/ColorPickupFlyout.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "ColorPickupFlyout.g.h"
+#include "../cascadia/inc/cppwinrt_utils.h"
+
+namespace winrt::TerminalApp::implementation
+{
+    struct ColorPickupFlyout : ColorPickupFlyoutT<ColorPickupFlyout>
+    {
+        ColorPickupFlyout();
+
+        void ColorButton_Click(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+        void ShowColorPickerButton_Click(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+        void CustomColorButton_Click(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+        void ClearColorButton_Click(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+
+        DECLARE_EVENT(ColorSelected, _colorSelected, TerminalApp::ColorSelectedArgs);
+        DECLARE_EVENT(ColorCleared, _colorCleared, TerminalApp::ColorClearedArgs);
+
+    private:
+        void _ColorChangedHandler(const Windows::UI::Xaml::Controls::ColorPicker&, const Windows::UI::Xaml::Controls::ColorChangedEventArgs& args);
+    };
+}
+
+namespace winrt::TerminalApp::factory_implementation
+{
+    struct ColorPickupFlyout : ColorPickupFlyoutT<ColorPickupFlyout, implementation::ColorPickupFlyout>
+    {
+    };
+}
