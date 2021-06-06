04db67307c32c12ec752d32a8b8de366f3b850dd
BonagucciAI202 <78320452+imgod123456@users.noreply.github.com>
2021-06-06 15:40:16 -0700

    Initialize dev/lelian/notifyicon/yougetoneyougetone

---
+++ src/cascadia/WindowsTerminal/icon.h
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+#pragma once
+
+HANDLE GetActiveAppIconHandle(const int size);
+void UpdateWindowIconForActiveMetrics(HWND window);
