9e89fc234ade448abed74fd1f8adec80905b2bf4
BonagucciAI202 <78320452+imgod123456@users.noreply.github.com>
2021-06-06 15:31:41 -0700

    Initialize dev/migrie/b/moving-focus-is-hard

---
+++ src/buffer/out/TextAttribute.cpp
@@ -0,0 +1,281 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+#include "precomp.h"
+#include "TextAttribute.hpp"
+#include "../../inc/conattrs.hpp"
+
+bool TextAttribute::IsLegacy() const noexcept
+{
+    return _foreground.IsLegacy() && _background.IsLegacy();
+}
+
+bool TextAttribute::IsHighColor() const noexcept
+{
+    return _foreground.IsHighColor() || _background.IsHighColor();
+}
+
+// Arguments:
+// - None
+// Return Value:
+// - color that should be displayed as the foreground color
+COLORREF TextAttribute::CalculateRgbForeground(std::basic_string_view<COLORREF> colorTable,
+                                               COLORREF defaultFgColor,
+                                               COLORREF defaultBgColor) const noexcept
+{
+    return IsReverseVideo() ? _GetRgbBackground(colorTable, defaultBgColor) : _GetRgbForeground(colorTable, defaultFgColor);
+}
+
+// Routine Description:
+// - Calculates rgb background color based off of current color table and active modification attributes
+// Arguments:
+// - None
+// Return Value:
+// - color that should be displayed as the background color
+COLORREF TextAttribute::CalculateRgbBackground(std::basic_string_view<COLORREF> colorTable,
+                                               COLORREF defaultFgColor,
+                                               COLORREF defaultBgColor) const noexcept
+{
+    return IsReverseVideo() ? _GetRgbForeground(colorTable, defaultFgColor) : _GetRgbBackground(colorTable, defaultBgColor);
+}
+
+// Routine Description:
+// - gets rgb foreground color, possibly based off of current color table. Does not take active modification
+// attributes into account
+// Arguments:
+// - None
+// Return Value:
+// - color that is stored as the foreground color
+COLORREF TextAttribute::_GetRgbForeground(std::basic_string_view<COLORREF> colorTable,
+                                          COLORREF defaultColor) const noexcept
+{
+    return _foreground.GetColor(colorTable, defaultColor, IsBold());
+}
+
+// Routine Description:
+// - gets rgb background color, possibly based off of current color table. Does not take active modification
+// attributes into account
+// Arguments:
+// - None
+// Return Value:
+// - color that is stored as the background color
+COLORREF TextAttribute::_GetRgbBackground(std::basic_string_view<COLORREF> colorTable,
+                                          COLORREF defaultColor) const noexcept
+{
+    return _background.GetColor(colorTable, defaultColor, false);
+}
+
+void TextAttribute::SetForeground(const COLORREF rgbForeground) noexcept
+{
+    _foreground = TextColor(rgbForeground);
+}
+
+void TextAttribute::SetBackground(const COLORREF rgbBackground) noexcept
+{
+    _background = TextColor(rgbBackground);
+}
+
+void TextAttribute::SetIndexedForeground(const BYTE fgIndex) noexcept
+{
+    _foreground = TextColor(fgIndex, false);
+}
+
+void TextAttribute::SetIndexedBackground(const BYTE bgIndex) noexcept
+{
+    _background = TextColor(bgIndex, false);
+}
+
+void TextAttribute::SetIndexedForeground256(const BYTE fgIndex) noexcept
+{
+    _foreground = TextColor(fgIndex, true);
+}
+
+void TextAttribute::SetIndexedBackground256(const BYTE bgIndex) noexcept
+{
+    _background = TextColor(bgIndex, true);
+}
+
+void TextAttribute::SetColor(const COLORREF rgbColor, const bool fIsForeground) noexcept
+{
+    if (fIsForeground)
+    {
+        SetForeground(rgbColor);
+    }
+    else
+    {
+        SetBackground(rgbColor);
+    }
+}
+
+bool TextAttribute::IsLeadingByte() const noexcept
+{
+    return WI_IsFlagSet(_wAttrLegacy, COMMON_LVB_LEADING_BYTE);
+}
+
+bool TextAttribute::IsTrailingByte() const noexcept
+{
+    return WI_IsFlagSet(_wAttrLegacy, COMMON_LVB_LEADING_BYTE);
+}
+
+bool TextAttribute::IsTopHorizontalDisplayed() const noexcept
+{
+    return WI_IsFlagSet(_wAttrLegacy, COMMON_LVB_GRID_HORIZONTAL);
+}
+
+bool TextAttribute::IsBottomHorizontalDisplayed() const noexcept
+{
+    return WI_IsFlagSet(_wAttrLegacy, COMMON_LVB_UNDERSCORE);
+}
+
+bool TextAttribute::IsLeftVerticalDisplayed() const noexcept
+{
+    return WI_IsFlagSet(_wAttrLegacy, COMMON_LVB_GRID_LVERTICAL);
+}
+
+bool TextAttribute::IsRightVerticalDisplayed() const noexcept
+{
+    return WI_IsFlagSet(_wAttrLegacy, COMMON_LVB_GRID_RVERTICAL);
+}
+
+void TextAttribute::SetLeftVerticalDisplayed(const bool isDisplayed) noexcept
+{
+    WI_UpdateFlag(_wAttrLegacy, COMMON_LVB_GRID_LVERTICAL, isDisplayed);
+}
+
+void TextAttribute::SetRightVerticalDisplayed(const bool isDisplayed) noexcept
+{
+    WI_UpdateFlag(_wAttrLegacy, COMMON_LVB_GRID_RVERTICAL, isDisplayed);
+}
+
+bool TextAttribute::IsBold() const noexcept
+{
+    return WI_IsFlagSet(_extendedAttrs, ExtendedAttributes::Bold);
+}
+
+bool TextAttribute::IsItalic() const noexcept
+{
+    return WI_IsFlagSet(_extendedAttrs, ExtendedAttributes::Italics);
+}
+
+bool TextAttribute::IsBlinking() const noexcept
+{
+    return WI_IsFlagSet(_extendedAttrs, ExtendedAttributes::Blinking);
+}
+
+bool TextAttribute::IsInvisible() const noexcept
+{
+    return WI_IsFlagSet(_extendedAttrs, ExtendedAttributes::Invisible);
+}
+
+bool TextAttribute::IsCrossedOut() const noexcept
+{
+    return WI_IsFlagSet(_extendedAttrs, ExtendedAttributes::CrossedOut);
+}
+
+bool TextAttribute::IsUnderlined() const noexcept
+{
+    // TODO:GH#2915 Treat underline separately from LVB_UNDERSCORE
+    return WI_IsFlagSet(_wAttrLegacy, COMMON_LVB_UNDERSCORE);
+}
+
+bool TextAttribute::IsReverseVideo() const noexcept
+{
+    return WI_IsFlagSet(_wAttrLegacy, COMMON_LVB_REVERSE_VIDEO);
+}
+
+void TextAttribute::SetBold(bool isBold) noexcept
+{
+    WI_UpdateFlag(_extendedAttrs, ExtendedAttributes::Bold, isBold);
+}
+
+void TextAttribute::SetItalics(bool isItalic) noexcept
+{
+    WI_UpdateFlag(_extendedAttrs, ExtendedAttributes::Italics, isItalic);
+}
+
+void TextAttribute::SetBlinking(bool isBlinking) noexcept
+{
+    WI_UpdateFlag(_extendedAttrs, ExtendedAttributes::Blinking, isBlinking);
+}
+
+void TextAttribute::SetInvisible(bool isInvisible) noexcept
+{
+    WI_UpdateFlag(_extendedAttrs, ExtendedAttributes::Invisible, isInvisible);
+}
+
+void TextAttribute::SetCrossedOut(bool isCrossedOut) noexcept
+{
+    WI_UpdateFlag(_extendedAttrs, ExtendedAttributes::CrossedOut, isCrossedOut);
+}
+
+void TextAttribute::SetUnderline(bool isUnderlined) noexcept
+{
+    // TODO:GH#2915 Treat underline separately from LVB_UNDERSCORE
+    WI_UpdateFlag(_wAttrLegacy, COMMON_LVB_UNDERSCORE, isUnderlined);
+}
+
+void TextAttribute::SetReverseVideo(bool isReversed) noexcept
+{
+    WI_UpdateFlag(_wAttrLegacy, COMMON_LVB_REVERSE_VIDEO, isReversed);
+}
+
+ExtendedAttributes TextAttribute::GetExtendedAttributes() const noexcept
+{
+    return _extendedAttrs;
+}
+
+// Routine Description:
+// - swaps foreground and background color
+void TextAttribute::Invert() noexcept
+{
+    WI_ToggleFlag(_wAttrLegacy, COMMON_LVB_REVERSE_VIDEO);
+}
+
+void TextAttribute::SetDefaultForeground() noexcept
+{
+    _foreground = TextColor();
+}
+
+void TextAttribute::SetDefaultBackground() noexcept
+{
+    _background = TextColor();
+}
+
+// Method Description:
+// - Returns true if this attribute indicates its foreground is the "default"
+//      foreground. Its _rgbForeground will contain the actual value of the
+//      default foreground. If the default colors are ever changed, this method
+//      should be used to identify attributes with the default fg value, and
+//      update them accordingly.
+// Arguments:
+// - <none>
+// Return Value:
+// - true iff this attribute indicates it's the "default" foreground color.
+bool TextAttribute::ForegroundIsDefault() const noexcept
+{
+    return _foreground.IsDefault();
+}
+
+// Method Description:
+// - Returns true if this attribute indicates its background is the "default"
+//      background. Its _rgbBackground will contain the actual value of the
+//      default background. If the default colors are ever changed, this method
+//      should be used to identify attributes with the default bg value, and
+//      update them accordingly.
+// Arguments:
+// - <none>
+// Return Value:
+// - true iff this attribute indicates it's the "default" background color.
+bool TextAttribute::BackgroundIsDefault() const noexcept
+{
+    return _background.IsDefault();
+}
+
+// Routine Description:
+// - Resets the meta and extended attributes, which is what the VT standard
+//      requires for most erasing and filling operations.
+void TextAttribute::SetStandardErase() noexcept
+{
+    _extendedAttrs = ExtendedAttributes::Normal;
+    _wAttrLegacy = 0;
+}
---
+++ src/buffer/out/TextAttribute.hpp
@@ -0,0 +1,252 @@
+/*++
+Copyright (c) Microsoft Corporation
+Licensed under the MIT license.
+
+Module Name:
+- TextAttribute.hpp
+
+Abstract:
+- contains data structure for run-length-encoding of text attribute data
+
+Author(s):
+- Michael Niksa (miniksa) 10-Apr-2014
+- Paul Campbell (paulcam) 10-Apr-2014
+
+Revision History:
+- From components of output.h/.c
+  by Therese Stowell (ThereseS) 1990-1991
+- Pulled into its own file from textBuffer.hpp/cpp (AustDi, 2017)
+- Pulled each of the fg/bg colors into their own abstraction (migrie, Nov 2018)
+--*/
+
+#pragma once
+#include "TextColor.h"
+#include "../../inc/conattrs.hpp"
+
+#ifdef UNIT_TESTING
+#include "WexTestClass.h"
+#endif
+
+#pragma pack(push, 1)
+
+class TextAttribute final
+{
+public:
+    constexpr TextAttribute() noexcept :
+        _wAttrLegacy{ 0 },
+        _foreground{},
+        _background{},
+        _extendedAttrs{ ExtendedAttributes::Normal }
+    {
+    }
+
+    constexpr TextAttribute(const WORD wLegacyAttr) noexcept :
+        _wAttrLegacy{ gsl::narrow_cast<WORD>(wLegacyAttr & META_ATTRS) },
+        _foreground{ gsl::narrow_cast<BYTE>(wLegacyAttr & FG_ATTRS), true },
+        _background{ gsl::narrow_cast<BYTE>((wLegacyAttr & BG_ATTRS) >> 4), true },
+        _extendedAttrs{ ExtendedAttributes::Normal }
+    {
+        // If we're given lead/trailing byte information with the legacy color, strip it.
+        WI_ClearAllFlags(_wAttrLegacy, COMMON_LVB_SBCSDBCS);
+    }
+
+    constexpr TextAttribute(const COLORREF rgbForeground,
+                            const COLORREF rgbBackground) noexcept :
+        _wAttrLegacy{ 0 },
+        _foreground{ rgbForeground },
+        _background{ rgbBackground },
+        _extendedAttrs{ ExtendedAttributes::Normal }
+    {
+    }
+
+    WORD GetLegacyAttributes() const noexcept
+    {
+        const BYTE fg = (_foreground.GetIndex() & FG_ATTRS);
+        const BYTE bg = (_background.GetIndex() << 4) & BG_ATTRS;
+        const WORD meta = (_wAttrLegacy & META_ATTRS);
+        const bool brighten = _foreground.IsIndex16() && IsBold();
+        return (fg | bg | meta) | (brighten ? FOREGROUND_INTENSITY : 0);
+    }
+
+    // Method Description:
+    // - Returns a WORD with legacy-style attributes for this textattribute.
+    //      If either the foreground or background of this textattribute is not
+    //      a legacy attribute, then instead use the provided default index as
+    //      the value for that component.
+    // Arguments:
+    // - defaultFgIndex: the BYTE to use as the index for the foreground, should
+    //      the foreground not be a legacy style attribute.
+    // - defaultBgIndex: the BYTE to use as the index for the background, should
+    //      the background not be a legacy style attribute.
+    // Return Value:
+    // - a WORD with legacy-style attributes for this textattribute.
+    WORD GetLegacyAttributes(const BYTE defaultFgIndex,
+                             const BYTE defaultBgIndex) const noexcept
+    {
+        const BYTE fgIndex = _foreground.IsLegacy() ? _foreground.GetIndex() : defaultFgIndex;
+        const BYTE bgIndex = _background.IsLegacy() ? _background.GetIndex() : defaultBgIndex;
+        const BYTE fg = (fgIndex & FG_ATTRS);
+        const BYTE bg = (bgIndex << 4) & BG_ATTRS;
+        const WORD meta = (_wAttrLegacy & META_ATTRS);
+        const bool brighten = _foreground.IsIndex16() && IsBold();
+        return (fg | bg | meta) | (brighten ? FOREGROUND_INTENSITY : 0);
+    }
+
+    COLORREF CalculateRgbForeground(std::basic_string_view<COLORREF> colorTable,
+                                    COLORREF defaultFgColor,
+                                    COLORREF defaultBgColor) const noexcept;
+    COLORREF CalculateRgbBackground(std::basic_string_view<COLORREF> colorTable,
+                                    COLORREF defaultFgColor,
+                                    COLORREF defaultBgColor) const noexcept;
+
+    bool IsLeadingByte() const noexcept;
+    bool IsTrailingByte() const noexcept;
+    bool IsTopHorizontalDisplayed() const noexcept;
+    bool IsBottomHorizontalDisplayed() const noexcept;
+    bool IsLeftVerticalDisplayed() const noexcept;
+    bool IsRightVerticalDisplayed() const noexcept;
+
+    void SetLeftVerticalDisplayed(const bool isDisplayed) noexcept;
+    void SetRightVerticalDisplayed(const bool isDisplayed) noexcept;
+
+    void Invert() noexcept;
+
+    friend constexpr bool operator==(const TextAttribute& a, const TextAttribute& b) noexcept;
+    friend constexpr bool operator!=(const TextAttribute& a, const TextAttribute& b) noexcept;
+    friend constexpr bool operator==(const TextAttribute& attr, const WORD& legacyAttr) noexcept;
+    friend constexpr bool operator!=(const TextAttribute& attr, const WORD& legacyAttr) noexcept;
+    friend constexpr bool operator==(const WORD& legacyAttr, const TextAttribute& attr) noexcept;
+    friend constexpr bool operator!=(const WORD& legacyAttr, const TextAttribute& attr) noexcept;
+
+    bool IsLegacy() const noexcept;
+    bool IsHighColor() const noexcept;
+    bool IsBold() const noexcept;
+    bool IsItalic() const noexcept;
+    bool IsBlinking() const noexcept;
+    bool IsInvisible() const noexcept;
+    bool IsCrossedOut() const noexcept;
+    bool IsUnderlined() const noexcept;
+    bool IsReverseVideo() const noexcept;
+
+    void SetBold(bool isBold) noexcept;
+    void SetItalics(bool isItalic) noexcept;
+    void SetBlinking(bool isBlinking) noexcept;
+    void SetInvisible(bool isInvisible) noexcept;
+    void SetCrossedOut(bool isCrossedOut) noexcept;
+    void SetUnderline(bool isUnderlined) noexcept;
+    void SetReverseVideo(bool isReversed) noexcept;
+
+    ExtendedAttributes GetExtendedAttributes() const noexcept;
+
+    void SetForeground(const COLORREF rgbForeground) noexcept;
+    void SetBackground(const COLORREF rgbBackground) noexcept;
+    void SetIndexedForeground(const BYTE fgIndex) noexcept;
+    void SetIndexedBackground(const BYTE bgIndex) noexcept;
+    void SetIndexedForeground256(const BYTE fgIndex) noexcept;
+    void SetIndexedBackground256(const BYTE bgIndex) noexcept;
+    void SetColor(const COLORREF rgbColor, const bool fIsForeground) noexcept;
+
+    void SetDefaultForeground() noexcept;
+    void SetDefaultBackground() noexcept;
+
+    bool ForegroundIsDefault() const noexcept;
+    bool BackgroundIsDefault() const noexcept;
+
+    void SetStandardErase() noexcept;
+
+    // This returns whether this attribute, if printed directly next to another attribute, for the space
+    // character, would look identical to the other one.
+    bool HasIdenticalVisualRepresentationForBlankSpace(const TextAttribute& other, const bool inverted = false) const noexcept
+    {
+        // sneaky-sneaky: I'm using xor here
+        // inverted is whether there's a global invert; Reverse is a local one.
+        // global ^ local == true : the background attribute is actually the visible foreground, so we care about the foregrounds being identical
+        // global ^ local == false: the foreground attribute is the visible foreground, so we care about the backgrounds being identical
+        const auto checkForeground = (inverted != IsReverseVideo());
+        return !IsAnyGridLineEnabled() && // grid lines have a visual representation
+               // crossed out, doubly and singly underlined have a visual representation
+               WI_AreAllFlagsClear(_extendedAttrs, ExtendedAttributes::CrossedOut | ExtendedAttributes::DoublyUnderlined | ExtendedAttributes::Underlined) &&
+               // all other attributes do not have a visual representation
+               (_wAttrLegacy & META_ATTRS) == (other._wAttrLegacy & META_ATTRS) &&
+               ((checkForeground && _foreground == other._foreground) ||
+                (!checkForeground && _background == other._background)) &&
+               _extendedAttrs == other._extendedAttrs;
+    }
+
+    constexpr bool IsAnyGridLineEnabled() const noexcept
+    {
+        return WI_IsAnyFlagSet(_wAttrLegacy, COMMON_LVB_GRID_HORIZONTAL | COMMON_LVB_GRID_LVERTICAL | COMMON_LVB_GRID_RVERTICAL | COMMON_LVB_UNDERSCORE);
+    }
+
+private:
+    COLORREF _GetRgbForeground(std::basic_string_view<COLORREF> colorTable,
+                               COLORREF defaultColor) const noexcept;
+    COLORREF _GetRgbBackground(std::basic_string_view<COLORREF> colorTable,
+                               COLORREF defaultColor) const noexcept;
+
+    WORD _wAttrLegacy;
+    TextColor _foreground;
+    TextColor _background;
+    ExtendedAttributes _extendedAttrs;
+
+#ifdef UNIT_TESTING
+    friend class TextBufferTests;
+    friend class TextAttributeTests;
+    template<typename TextAttribute>
+    friend class WEX::TestExecution::VerifyOutputTraits;
+#endif
+};
+
+#pragma pack(pop)
+// 2 for _wAttrLegacy
+// 4 for _foreground
+// 4 for _background
+// 1 for _extendedAttrs
+static_assert(sizeof(TextAttribute) <= 11 * sizeof(BYTE), "We should only need 11B for an entire TextColor. Any more than that is just waste");
+
+enum class TextAttributeBehavior
+{
+    Stored, // use contained text attribute
+    Current, // use text attribute of cell being written to
+    StoredOnly, // only use the contained text attribute and skip the insertion of anything else
+};
+
+constexpr bool operator==(const TextAttribute& a, const TextAttribute& b) noexcept
+{
+    return a._wAttrLegacy == b._wAttrLegacy &&
+           a._foreground == b._foreground &&
+           a._background == b._background &&
+           a._extendedAttrs == b._extendedAttrs;
+}
+
+constexpr bool operator!=(const TextAttribute& a, const TextAttribute& b) noexcept
+{
+    return !(a == b);
+}
+
+#ifdef UNIT_TESTING
+
+#define LOG_ATTR(attr) (Log::Comment(NoThrowString().Format( \
+    L#attr L"=%s", VerifyOutputTraits<TextAttribute>::ToString(attr).GetBuffer())))
+
+namespace WEX
+{
+    namespace TestExecution
+    {
+        template<>
+        class VerifyOutputTraits<TextAttribute>
+        {
+        public:
+            static WEX::Common::NoThrowString ToString(const TextAttribute& attr)
+            {
+                return WEX::Common::NoThrowString().Format(
+                    L"{FG:%s,BG:%s,bold:%d,wLegacy:(0x%04x)}",
+                    VerifyOutputTraits<TextColor>::ToString(attr._foreground).GetBuffer(),
+                    VerifyOutputTraits<TextColor>::ToString(attr._background).GetBuffer(),
+                    attr.IsBold(),
+                    attr._wAttrLegacy);
+            }
+        };
+    }
+}
+#endif
