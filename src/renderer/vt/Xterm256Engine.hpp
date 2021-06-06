c8d3b2a92f52304965460b734bc75ff7c7993af2
BonagucciAI202 <78320452+imgod123456@users.noreply.github.com>
2021-06-06 15:32:11 -0700

    Initialize dev/miniksa/gotta_go_fast

---
+++ src/renderer/vt/Xterm256Engine.hpp
@@ -0,0 +1,53 @@
+/*++
+Copyright (c) Microsoft Corporation
+Licensed under the MIT license.
+
+Module Name:
+- Xterm256Engine.hpp
+
+Abstract:
+- This is the definition of the VT specific implementation of the renderer.
+    This is the xterm-256color implementation, which supports advanced sequences such as
+    inserting and deleting lines, and true rgb color.
+
+Author(s):
+- Mike Griese (migrie) 01-Sept-2017
+--*/
+
+#pragma once
+
+#include "XtermEngine.hpp"
+
+namespace Microsoft::Console::Render
+{
+    class Xterm256Engine : public XtermEngine
+    {
+    public:
+        Xterm256Engine(_In_ wil::unique_hfile hPipe,
+                       const Microsoft::Console::IDefaultColorProvider& colorProvider,
+                       const Microsoft::Console::Types::Viewport initialViewport,
+                       const std::basic_string_view<COLORREF> colorTable);
+
+        virtual ~Xterm256Engine() override = default;
+
+        [[nodiscard]] HRESULT UpdateDrawingBrushes(const COLORREF colorForeground,
+                                                   const COLORREF colorBackground,
+                                                   const WORD legacyColorAttribute,
+                                                   const ExtendedAttributes extendedAttrs,
+                                                   const bool isSettingDefaultBrushes) noexcept override;
+
+        [[nodiscard]] HRESULT ManuallyClearScrollback() noexcept override;
+
+    private:
+        [[nodiscard]] HRESULT _UpdateExtendedAttrs(const ExtendedAttributes extendedAttrs) noexcept;
+
+        // We're only using Italics, Blinking, Invisible and Crossed Out for now
+        // See GH#2916 for adding a more complete implementation.
+        ExtendedAttributes _lastExtendedAttrsState;
+
+#ifdef UNIT_TESTING
+        friend class VtRendererTest;
+        friend class ConptyOutputTests;
+#endif
+    };
+}
