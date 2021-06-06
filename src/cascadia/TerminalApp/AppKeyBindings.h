ac478aa210bc61d37b6205c3f1b0e99d68306496
BonagucciAI202 <78320452+imgod123456@users.noreply.github.com>
2021-06-06 15:34:34 -0700

    Initialize release-1.4

---
+++ src/cascadia/TerminalApp/AppKeyBindings.h
@@ -0,0 +1,44 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+#pragma once
+
+#include "AppKeyBindings.g.h"
+#include "ActionArgs.h"
+#include "ShortcutActionDispatch.h"
+#include "..\inc\cppwinrt_utils.h"
+
+// fwdecl unittest classes
+namespace TerminalAppLocalTests
+{
+    class SettingsTests;
+    class KeyBindingsTests;
+    class TestUtils;
+}
+
+namespace winrt::TerminalApp::implementation
+{
+    struct AppKeyBindings : AppKeyBindingsT<AppKeyBindings>
+    {
+        AppKeyBindings() = default;
+
+        bool TryKeyChord(winrt::Microsoft::Terminal::TerminalControl::KeyChord const& kc);
+
+        void SetDispatch(const winrt::TerminalApp::ShortcutActionDispatch& dispatch);
+        void SetKeyMapping(const winrt::TerminalApp::KeyMapping& keymap);
+
+    private:
+        winrt::TerminalApp::KeyMapping _keymap{ nullptr };
+
+        winrt::TerminalApp::ShortcutActionDispatch _dispatch{ nullptr };
+
+        friend class TerminalAppLocalTests::SettingsTests;
+        friend class TerminalAppLocalTests::KeyBindingsTests;
+        friend class TerminalAppLocalTests::TestUtils;
+    };
+}
+
+namespace winrt::TerminalApp::factory_implementation
+{
+    BASIC_FACTORY(AppKeyBindings);
+}
