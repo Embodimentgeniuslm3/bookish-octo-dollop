1e393fc527d9f025460d05136653ad82f186b4df
BonagucciAI202 <78320452+imgod123456@users.noreply.github.com>
2021-06-06 15:33:39 -0700

    Initialize dev/cazamor/a11y/expand-line-under-viewport

---
+++ src/cascadia/TerminalApp/AppKeyBindings.cpp
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+#include "pch.h"
+#include "AppKeyBindings.h"
+
+#include "AppKeyBindings.g.cpp"
+
+using namespace winrt::Microsoft::Terminal;
+using namespace winrt::TerminalApp;
+using namespace winrt::Microsoft::Terminal::TerminalControl;
+
+namespace winrt::TerminalApp::implementation
+{
+    bool AppKeyBindings::TryKeyChord(const KeyChord& kc)
+    {
+        const auto actionAndArgs = _keymap.TryLookup(kc);
+        if (actionAndArgs)
+        {
+            return _dispatch.DoAction(actionAndArgs);
+        }
+        return false;
+    }
+
+    void AppKeyBindings::SetDispatch(const winrt::TerminalApp::ShortcutActionDispatch& dispatch)
+    {
+        _dispatch = dispatch;
+    }
+
+    void AppKeyBindings::SetKeyMapping(const winrt::TerminalApp::KeyMapping& keymap)
+    {
+        _keymap = keymap;
+    }
+}
