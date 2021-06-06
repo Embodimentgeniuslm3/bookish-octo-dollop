436a31ece922359def7a9c42c35cd40e5a501b50
BonagucciAI202 <78320452+imgod123456@users.noreply.github.com>
2021-06-06 15:34:01 -0700

    Initialize dev/migrie/oop-window-content-1

---
+++ src/tools/ScratchWinRTServer/HostClass.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "HostClass.g.h"
+#include <winrt/Microsoft.Terminal.TerminalConnection.h>
+#include <winrt/Microsoft.Terminal.Settings.h>
+#include "../../renderer/base/Renderer.hpp"
+#include "../../renderer/dx/DxRenderer.hpp"
+#include "../../renderer/uia/UiaRenderer.hpp"
+#include "../../cascadia/TerminalCore/Terminal.hpp"
+
+namespace winrt::ScratchWinRTServer::implementation
+{
+    struct HostClass : HostClassT<HostClass>
+    {
+        HostClass(const winrt::guid& g);
+        ~HostClass();
+        void DoTheThing();
+
+        void Attach(Windows::UI::Xaml::Controls::SwapChainPanel panel);
+        void BeginRendering();
+
+        void RenderEngineSwapChainChanged();
+
+        void ThisIsInsane(uint64_t swapchainHandle);
+
+    private:
+        int _DoCount{ 0 };
+        winrt::guid _id;
+        wil::unique_handle _hSwapchain{ INVALID_HANDLE_VALUE };
+        Windows::UI::Xaml::Controls::SwapChainPanel _panel{ nullptr };
+
+        bool _initializedTerminal{ false };
+        winrt::Microsoft::Terminal::TerminalConnection::ITerminalConnection _connection;
+        std::unique_ptr<::Microsoft::Terminal::Core::Terminal> _terminal;
+        FontInfoDesired _desiredFont;
+        FontInfo _actualFont;
+
+        std::unique_ptr<::Microsoft::Console::Render::Renderer> _renderer;
+        std::unique_ptr<::Microsoft::Console::Render::DxEngine> _renderEngine;
+        winrt::Microsoft::Terminal::Settings::IControlSettings _settings;
+        void _AttachDxgiSwapChainToXaml(HANDLE swapChainHandle);
+
+        bool _InitializeTerminal();
+    };
+}
+
+namespace winrt::ScratchWinRTServer::factory_implementation
+{
+    struct HostClass : HostClassT<HostClass, implementation::HostClass>
+    {
+    };
+}
